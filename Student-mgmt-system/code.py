63588d2d79e8e81b7efc23ada24e9953977c762e
poonam-tanwar <poonam-tanwar@users.noreply.github.com>
2020-06-16 23:51:32 +0530

    Code file created.

---
+++ Student-mgmt-system/code.py
@@ -0,0 +1,86 @@
+# --------------
+# Code starts here
+
+# Create the lists 
+
+# Concatenate both the strings
+
+
+# Append the list
+
+# Print updated list
+
+
+# Remove the element from the list
+
+# Print the list
+
+
+
+# Create the Dictionary
+
+
+
+# Slice the dict and stores  the all subjects marks in variable
+
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+
+# Insert percentage formula
+
+# Print the percentage
+
+
+
+
+# Create the Dictionary
+ 
+
+
+
+# Given string
+class_1=['Geoffrey','Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2=['Hilary Mason','Carla Gentry','Corinna Cortes']
+new_class= class_1+class_2
+print(new_class)
+new_class.append('Peter Warden')
+print(new_class)
+new_class.remove('Carla Gentry')
+print(new_class)
+
+courses={'Math':65,'English':70,'History':80,'French':70,'Science':60}
+total= sum(courses.values())
+print(total)
+percentage=(total/500)*100
+print(percentage)
+
+mathematics={'Geoffrey Hinton': 78,'Andrew Ng':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
+max_marks_scored=max(mathematics,key= mathematics.get)
+print(max_marks_scored)
+
+topper= 'Andrew Ng'
+topper.split()
+first_name=topper.split()[0]
+print(first_name)
+last_name=topper.split()[1]
+print(last_name)
+full_name=last_name+' '+first_name
+print(full_name)
+certificate_name=full_name.upper()
+print(certificate_name)
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
